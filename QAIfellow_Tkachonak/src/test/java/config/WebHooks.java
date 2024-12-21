e87ca42a4eaab396a72edf086a532b7c23c23e25
StanislavTkachonak <tkachonakstas@gmail.com>
2024-12-21 23:26:41 +0300

    HWF_UI

--- QAIfellow_Tkachonak/src/test/java/config/WebHooks.java
+++ QAIfellow_Tkachonak/src/test/java/config/WebHooks.java
@@ -8,11 +8,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 import static com.codeborne.selenide.Selenide.open;
+import static config.Configuration.getConfigurationValue;
 
 public class WebHooks {
 
@@ -28,20 +25,9 @@ public class WebHooks {
     }
 
     @BeforeEach
-    public void initBrowser() throws IOException {
-        Properties properties = new Properties();
-        FileInputStream fis = new FileInputStream("src/test/resources/test.properties");
-        properties.load(fis);
-        String chromeDriverPath129 = properties.getProperty("chrome.driver.version.129");
-        String chromeDriverPath131 = properties.getProperty("chrome.driver.version.131");
-        String chromeVersion = properties.getProperty("webdriverVersion");
-        if ("129".equals(chromeVersion)) {
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath129);
-        } else if ("131".equals(chromeVersion)) {
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath131);
-        } else {
-            throw new IllegalArgumentException("Unsupported Chrome version: " + chromeVersion);
-        }
+    public void initBrowser() {
+        String webDriverLocation = getConfigurationValue("webdriverlocation");
+        System.setProperty("webdriver.chrome.driver", webDriverLocation);
         Configuration.browser = "chrome";
         Configuration.startMaximized = true;
         open("https://edujira.ifellow.ru/");
--- QAIfellow_Tkachonak/src/test/java/test/EdujiraSearchTest.java
+++ QAIfellow_Tkachonak/src/test/java/test/EdujiraSearchTest.java
@@ -7,40 +7,46 @@ import steps.EdujiraProjectStep;
 import steps.EdujiraStartStep;
 import steps.EdujiraTestSeleniumATHomeworkStep;
 
+import static config.Configuration.getConfigurationValue;
+
 public class EdujiraSearchTest extends WebHooks {
+
+    EdujiraStartStep edujiraStartStep = new EdujiraStartStep();
+    EdujiraProjectStep edujiraProjectStep = new EdujiraProjectStep();
+    EdujiraTestSeleniumATHomeworkStep edujiraTestSeleniumATHomeworkStep = new EdujiraTestSeleniumATHomeworkStep();
 
     @Test
     public void authorizationTest(){
-        new EdujiraStartStep().authorization(config.Configuration.getConfigurationValue("login"), config.Configuration.getConfigurationValue("password"));
+        edujiraStartStep.authorization(getConfigurationValue("login"), getConfigurationValue("password"));
     }
 
     @Test
     public void clickOnProjectsTest(){
-        new EdujiraStartStep().authorization(config.Configuration.getConfigurationValue("login"), config.Configuration.getConfigurationValue("password"));
-        new EdujiraProjectStep().clickOnProjects();
+        edujiraStartStep.authorization(getConfigurationValue("login"), getConfigurationValue("password"));
+        edujiraProjectStep.clickOnProjects();
     }
-    
+
     @Test
     public void searchTaskTest(){
-        new EdujiraStartStep().authorization(config.Configuration.getConfigurationValue("login"), config.Configuration.getConfigurationValue("password"));
-        int count = new EdujiraProjectStep().clickOnProjects();
-        new EdujiraProjectStep().searchTask("HW3", count);
+        edujiraStartStep.authorization(getConfigurationValue("login"), getConfigurationValue("password"));
+        int count = edujiraProjectStep.clickOnProjects();
+        edujiraProjectStep.searchTask("HW3", count);
     }
 
     @Test
     public void clickOnTestSeleniumTest(){
-        new EdujiraStartStep().authorization(config.Configuration.getConfigurationValue("login"), config.Configuration.getConfigurationValue("password"));
-        int count = new EdujiraProjectStep().clickOnProjects();
-        new EdujiraProjectStep().searchTask("HW3", count);
-        new EdujiraTestSeleniumATHomeworkStep().clickOnTestSelenium("TestSeleniumATHomework");
+        edujiraStartStep.authorization(getConfigurationValue("login"), getConfigurationValue("password"));
+        int count = edujiraProjectStep.clickOnProjects();
+        edujiraProjectStep.searchTask("HW3", count);
+        edujiraTestSeleniumATHomeworkStep.clickOnTestSelenium("TestSeleniumATHomework");
     }
 
     @Test
     public void checkAllTasksTest() {
-        new EdujiraStartStep().authorization(config.Configuration.getConfigurationValue("login"), config.Configuration.getConfigurationValue("password"));
-        int count = new EdujiraProjectStep().clickOnProjects();
-        new EdujiraProjectStep().searchTask("HW3", count);
-        new EdujiraTestSeleniumATHomeworkStep().clickOnTestSelenium("TestSeleniumATHomework");
+        edujiraStartStep.authorization(getConfigurationValue("login"), getConfigurationValue("password"));
+        int count = edujiraProjectStep.clickOnProjects();
+        edujiraProjectStep.searchTask("HW3", count);
+        edujiraTestSeleniumATHomeworkStep.clickOnTestSelenium("TestSeleniumATHomework");
         new EdujiraBagStep().createBag("HW3",
                 "Некорректное отображение текста на странице при использовании масштабирования");
     }
